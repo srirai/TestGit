8f46d9a2b995fa463ed519c17cb75c1e770da463
srirai <srinidhi.raichur@capgemini.com>
2022-04-25 11:52:50 +0200

    Add new method to master branch

--- src/TestFile.java
+++ src/TestFile.java
@@ -3,4 +3,8 @@ public class TestFile {
     public static void main(String args[]){
         System.out.println("Hello World");
     }
+
+    public void newMethod(){
+        System.out.println("New commit in Master");
+    }
 }
